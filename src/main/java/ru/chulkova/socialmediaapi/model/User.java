83d8ede55494cd7aca174d4f3ead7002ffac4748
Elizaveta Chulkova <chulkova_liza@mail.ru>
2023-08-16 11:53:06 +0300

    Add security methods to User

--- src/main/java/ru/chulkova/socialmediaapi/model/User.java
+++ src/main/java/ru/chulkova/socialmediaapi/model/User.java
@@ -4,7 +4,11 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -13,7 +17,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User extends AbstractBaseEntity{
+public class User extends AbstractBaseEntity implements UserDetails {
 
     @Column(name = "name", nullable = false)
     private String name;
@@ -39,4 +43,37 @@ public class User extends AbstractBaseEntity{
             inverseJoinColumns = @JoinColumn(name = "subscriber_id", referencedColumnName = "id")
     )
     private List<User> subscribers;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
